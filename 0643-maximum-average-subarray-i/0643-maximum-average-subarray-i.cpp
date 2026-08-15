b04a71b199d7f31a87754f5ddce71bcd8add7a53
Durvesh Nilesh Pawar <durveshpawar2007@gmail.com>
2026-08-15 19:44:13 +0530

    Time: 0 ms (100%), Space: 113.7 MB (87.52%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
-    double findMaxAverage(std::vector<int>& nums, int k) {
-        int current_sum = 0;
-        for (int i = 0; i < k; ++i) {
-            current_sum += nums[i];
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int low = 0;
+        int high = k - 1;
+
+        int windowSum = 0;
+
+        
+        for (int i = low; i <= high; i++) {
+            windowSum += nums[i];
         }
-        int max_sum = current_sum;
-        for (int i = k; i < nums.size(); ++i) {
-            current_sum += nums[i] - nums[i - k];
-            max_sum = std::max(max_sum, current_sum);
+
+        int maxSum = windowSum;
+
+        
+        low++;
+        high++;
+
+        while (high < n) {
+            windowSum += nums[high] - nums[low - 1];
+
+            maxSum = max(maxSum, windowSum);
+
+            low++;
+            high++;
         }
-        return static_cast<double>(max_sum) / k;  
+
+        return (double)maxSum / k;
     }
 };
